e2a0bc310f44248c7e06ee465cd851cff896e114
jbreslin33 <jbreslin33@localhost>
2010-11-15 11:35:54 -0500

    ...

---
+++ Game.h
@@ -0,0 +1,32 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    Game.h
+*/
+#ifndef __Game_h_
+#define __Game_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+class GameServer;
+
+class Game
+{
+
+public:
+    Game();
+    virtual ~Game(void);
+
+
+	void setGameServer(GameServer* gameServer);
+
+protected:
+GameServer* gameServer;
+
+};
+
+#endif
+
+
