7b4bdddce3335914f0abf65cbb9a00e006ab1f5e
jbreslin33 <jbreslin33@localhost>
2010-11-23 22:55:21 -0500

    ..:wqi

---
+++ client/ClientGame.h
@@ -0,0 +1,39 @@
+/*
+-----------------------------------------------------------------------------
+Basic ClientGame class
+
+to get use out of this you want to extend it for a client game or server game then extend that for
+whatever your game is.
+
+The game itself should be another class completely such as pacman.h etc...QuestionFactory.h
+
+This should be left very generic and have lots of virtual functions and pointers that are set
+down the inheritance chain.
+
+Filename:    ClientGame.h
+*/
+#ifndef __ClientGame_h_
+#define __ClientGame_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include "../Game.h"
+
+class ClientGame : public Game
+{
+
+public:
+	ClientGame();
+    	virtual ~ClientGame(void);
+
+
+
+protected:
+
+
+};
+
+#endif
---
+++ client/questiongame/QuestionClientGame.h
@@ -0,0 +1,39 @@
+/*
+-----------------------------------------------------------------------------
+Basic QuestionClientGame class
+
+to get use out of this you want to extend it for a client game or server game then extend that for
+whatever your game is.
+
+The game itself should be another class completely such as pacman.h etc...QuestionFactory.h
+
+This should be left very generic and have lots of virtual functions and pointers that are set
+down the inheritance chain.
+
+Filename:    QuestionClientGame.h
+*/
+#ifndef __QuestionClientGame_h_
+#define __QuestionClientGame_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include "../ClientGame.h"
+
+class QuestionClientGame : public ClientGame
+{
+
+public:
+	QuestionClientGame();
+    	virtual ~QuestionClientGame(void);
+
+
+
+protected:
+
+
+};
+
+#endif
