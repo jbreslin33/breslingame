d28704a231557b5330526772e9bb49027d54e632
jbreslin33 <jbreslin33@localhost>
2010-11-29 16:27:58 -0500

    ..

--- listenserver/ListenServer.cpp
+++ listenserver/ListenServer.cpp
@@ -102,12 +102,12 @@ void ListenServer::processRequests()
     //printf("ipclient:%s",their_addr);
 
 
-char ip4[INET_ADDRSTRLEN];  // space to hold the IPv4 string
+//char ip4[INET_ADDRSTRLEN];  // space to hold the IPv4 string
 //struct sockaddr_in sa;      // pretend this is loaded with something
 
-inet_ntop(AF_INET, &(*their_addr.sin_addr), ip4, INET_ADDRSTRLEN);
+//inet_ntop(AF_INET, &(*their_addr.sin_addr), ip4, INET_ADDRSTRLEN);
 
-printf("The IPv4 address is: %s\n", ip4);
+//printf("The IPv4 address is: %s\n", ip4);
 
 
 
