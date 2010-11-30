ce93d84f12d40714324e1953438e87da58df2fd4
jbreslin33 <jbreslin33@localhost>
2010-11-30 00:23:57 -0500

    ..

--- simple/client.c
+++ simple/client.c
@@ -1,51 +1,59 @@
-      #include <arpa/inet.h>
-      #include <netinet/in.h>
-      #include <stdio.h>
-      #include <sys/types.h>
-      #include <sys/socket.h>
-      #include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <string.h>
 
-        #include <cstdlib>
-        #include <string.h>
+#define BUFLEN 512
+#define NPACK 10
+#define PORT 9930
 
-      #define BUFLEN 512
-      #define NPACK 10
-      #define PORT 9930
+#define SRV_IP "10.42.43.1"
 
-      #define SRV_IP "10.42.43.1"
-      /* diep(), #includes and #defines like in the server */
-
-      void diep(char *s)
-      {
-        perror(s);
+void diep(char *s)
+{
+	perror(s);
         exit(1);
-      }
+}
     
-      int main(void)
-      {
-        struct sockaddr_in si_other;
+int main(void)
+{
+	struct sockaddr_in; //my socket (client)
+	struct si_other;   //other socket (server)
+	
         int s, i, slen=sizeof(si_other);
         char buf[BUFLEN];
     
         if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-          diep("socket");
-    
-        memset((char *) &si_other, 0, sizeof(si_other));
+	{
+        	diep("socket");
+    	}
+        
+	memset((char *) &si_other, 0, sizeof(si_other));
         si_other.sin_family = AF_INET;
         si_other.sin_port = htons(PORT);
-        if (inet_aton(SRV_IP, &si_other.sin_addr)==0) {
-          fprintf(stderr, "inet_aton() failed\n");
-          exit(1);
+
+        if (inet_aton(SRV_IP, &si_other.sin_addr)==0)
+	{
+        	fprintf(stderr, "inet_aton() failed\n");
+          	exit(1);
         }
     
-        for (i=0; i<NPACK; i++) {
-          printf("Sending packet %d\n", i);
-          sprintf(buf, "This is packet %d\n", i);
-          if (sendto(s, buf, BUFLEN, 0, (struct sockaddr*)&si_other, slen)==-1)
-            diep("sendto()");
+	//loop and send 9 packets to a server
+        for (i=0; i<NPACK; i++)
+	{
+        	printf("Sending packet %d\n", i);  //just a printf to tell client what we are doing 
+          	sprintf(buf, "This is packet %d\n", i); //actual message
+          	if (sendto(s, buf, BUFLEN, 0, (struct sockaddr*)&si_other, slen)==-1) //this sends message
+		{
+            		diep("sendto()");
+		}
         }
     
-        close(s);
-        return 0;
+        close(s); //close socket
+        return 0; //return from main
       }
 
