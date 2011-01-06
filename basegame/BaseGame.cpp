1ff07b9fe7a566b1273820cd74af45abd497e73a
jbreslin33 <jbreslin33@localhost>
2011-01-06 12:42:06 -0500

    ....

---
+++ basegame/BaseGame.cpp
@@ -0,0 +1,77 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    BaseGame.cpp
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _ 
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/                              
+      Tutorial Framework
+      http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#include "BaseGame.h"
+
+//-------------------------------------------------------------------------------------
+BaseGame::BaseGame(void)
+{
+}
+//-------------------------------------------------------------------------------------
+BaseGame::~BaseGame(void)
+{
+}
+
+//-------------------------------------------------------------------------------------
+void BaseGame::createScene(void)
+{
+    // create your scene here :)
+}
+
+
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+
+		int time, oldTime, newTime;
+	MSG msg;
+	BOOL bMsg = FALSE;
+	PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE);
+
+//	oldTime = DreamSock_GetCurrentSystemTime();
+        // Create application object
+        BaseGame app;
+
+        try {
+            app.go();
+        } catch( Ogre::Exception& e ) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+            std::cerr << "An exception has occured: " <<
+                e.getFullDescription().c_str() << std::endl;
+#endif
+        }
+
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
