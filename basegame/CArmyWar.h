dd955b602c022d3db68b0cb2e020e13606ec9199
jbreslin33 <jbreslin33@localhost>
2011-01-16 18:27:20 -0500

    ....fixed keys for windows by not defining VK keys...

--- basegame/CArmyWar.h
+++ basegame/CArmyWar.h
@@ -192,13 +192,22 @@ public:
 	CArmyWar *next;
 
     bool keys[256];
+
+#ifdef WIN32
+	//do nothing
+#else
+
 #define VK_ESCAPE 0	
 #define VK_DOWN 1
 #define VK_UP 2
 #define VK_LEFT 3
 #define VK_RIGHT 4
 #define VK_SPACE 5
-//char* serverIP;
+#endif
+
+
+
+	//char* serverIP;
 	//std::vector<Character>* characterVector;
 
 	BaseGame* mBaseGame;
