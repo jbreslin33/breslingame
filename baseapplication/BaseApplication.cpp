b8fbc79622f692c405786967fce86a81020f4c5f
jbreslin33 <jbreslin33@localhost>
2011-01-20 10:06:22 -0500

    ..using the same createScene as sinbadController code for ogre source oddly the color is off and the position of ogre is always at top of screen. obviosly need to figure out why.

--- baseapplication/BaseApplication.cpp
+++ baseapplication/BaseApplication.cpp
@@ -148,7 +148,7 @@ void BaseApplication::destroyScene(void)
 void BaseApplication::createViewports(void)
 {
     // Create one viewport, entire window
-    Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+    vp = mWindow->addViewport(mCamera);
     vp->setBackgroundColour(Ogre::ColourValue(0,0,0));
 
     // Alter the camera aspect ratio to match the viewport
