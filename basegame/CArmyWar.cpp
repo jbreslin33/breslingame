aacb6da977b349ad9d8163903c1d461ac1dbc59d
jbreslin33 <jbreslin33@localhost>
2011-01-20 21:45:32 -0500

    ...trying to move with jkli keys 2nd client

--- basegame/CArmyWar.cpp
+++ basegame/CArmyWar.cpp
@@ -863,7 +863,7 @@ void CArmyWar::MoveObjects(void)
             		transVector.z = client->command.origin.y;
 			transVector.y = 5; //to keep player consistent y for now a hack
  
-			//client->character->getSceneNode()->setPosition(transVector);
+			client->character->getSceneNode()->setPosition(transVector);
 			
 			client->command.bullet.origin.x += client->serverFrame.bullet.vel.x * frametime;
 			client->command.bullet.origin.y += client->serverFrame.bullet.vel.y * frametime;
@@ -882,7 +882,7 @@ void CArmyWar::MoveObjects(void)
 		        transVector.y = 5; //to keep player consistent y for now
 	
 			//myCharacter->getSceneNode()->setPosition(transVector);
-			//client->character->getSceneNode()->setPosition(transVector);
+			client->character->getSceneNode()->setPosition(transVector);
 
 			client->command.bullet.origin.x = client->command.bullet.predictedOrigin.x;
 			client->command.bullet.origin.y = client->command.bullet.predictedOrigin.y;
