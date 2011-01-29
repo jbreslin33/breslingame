3967b72a1cb42b78c66834267253a3566af9788c
jbreslin33 <jbreslin33@localhost>
2011-01-29 18:10:10 -0500

    ..adding math library starting with vector3d

---
+++ math/Vector3D.cpp
@@ -0,0 +1,13 @@
+#include "Vector3D.h"
+
+Vector3D::Vector3D()
+{
+	x = 0;
+	y = 0;
+	z = 0;
+}
+
+Vector3D::~Vector3D()
+{
+
+}
---
+++ math/Vector3D.h
@@ -0,0 +1,13 @@
+#ifndef VECTOR3D_H
+#define VECTOR3D_H
+
+class Vector3D
+{
+
+Vector3D();
+~Vector3D();
+
+	float x;
+	float y;
+	float z;
+};
