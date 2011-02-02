7f6748e85779ec4c3392b8658052156ebf4b18c8
jbreslin33 <jbreslin33@localhost>
2011-02-02 14:14:45 -0500

    ...

--- game/ClientSideGame.h
+++ game/ClientSideGame.h
@@ -2,81 +2,15 @@
 #define CLIENTSIDEGAME_H
 
 #include "game.h"
-#include "../math/Vector3D.h"
-#include "../command/ClientSideCommand.h"
-#include "../client/ClientSideClient.h"
 
-//#include "Tutorial4.h"
-class DreamMessage;
-class DreamClient;
-class ClientSideNetwork;
-class BaseGame;
+class CArmyWar;
 
-#ifdef WIN32
-        //do nothing
-#else
-
-#define VK_ESCAPE 0
-#define VK_DOWN 1
-#define VK_UP 2
-#define VK_LEFT 3
-#define VK_RIGHT 4
-#define VK_SPACE 5
-#endif
-
-// The main application class interface
 class ClientSideGame : public Game
 {
 public:
-	void	DrawMap(void);
-
-	void	CheckPredictionError(int a);
-	void	CalculateVelocity(ClientSideCommand *command, float frametime);
-	void	PredictMovement(int prevFrame, int curFrame);
-	void	MoveObjects(void);
-
-	void	AddClient(int local, int index, char *name);
-	void	RemoveClient(int index);
-	void	RemoveClients(void);
-
-
-	ClientSideNetwork* mClientSideNetwork;
-
-	ClientSideClient *clientList;			// Client list
-	ClientSideClient *localClient;		// Pointer to the local client in the client list
-	int clients;
-
-	ClientSideClient inputClient;			// Handles all keyboard input
-
-	float frametime;
-
-	char gamename[32];
-	bool init;
-
-	int gameIndex;
-
-	ClientSideGame(BaseGame* baseGame);
+	ClientSideGame(CArmyWar* cArmyWar);
 	~ClientSideGame();
-
-	void	Shutdown(void);
-	void	CheckKeys(void);
-	void	Frame(void);
-
-	void	SetName(char *n)		{ strcpy(gamename, n); }
-	char	*GetName(void)			{ return gamename; }
-
-	void	SetGameIndex(int index)	{ gameIndex = index; }
-	int		GetGameIndex(void)		{ return gameIndex; }
-
-	DreamClient* networkClient;
-
-	ClientSideClient *GetClientList(void) { return clientList; }
-
-	ClientSideClient *GetClientPointer(int index);
-
-	ClientSideGame *next;
-
-	BaseGame* mBaseGame;
+CArmyWar* mCArmyWar;
 };
 
 #endif
--- network/clientSideNetwork.h
+++ network/clientSideNetwork.h
@@ -2,20 +2,6 @@
 #define CLIENTSIDENETWORK_H
 
 #include "network.h"
-#include "../client/ClientSideClient.h"
-#include "../dreamsock/DreamMessage.h"
-
-
-
-typedef struct clientLoginData
-{
-        int                                     index;
-        char                            nickname[30];
-        clientLoginData *next;
-} clientLoginData;
-
-extern char serverIP[32];
-
 
 class ClientSideGame;
 
@@ -23,25 +9,10 @@ class ClientSideNetwork : public Network
 {
 public:
 
-ClientSideNetwork(ClientSideGame* game);
+ClientSideNetwork(ClientSideGame* clientSideGame);
 ~ClientSideNetwork();
 
-// Network.cpp
-void ReadPackets(void);
-void SendCommand(void);
-void SendRequestNonDeltaFrame(void);
-void ReadMoveCommand(DreamMessage *mes, ClientSideClient *client);
-void ReadDeltaMoveCommand(DreamMessage *mes, ClientSideClient *client);
-void BuildDeltaMoveCommand(DreamMessage *mes, ClientSideClient *theClient);
-
-void RunNetwork(int msec);
-
-        // Network.cpp
-        void    StartConnection();
-        void    Connect(void);
-        void    Disconnect(void);
-        void    SendStartGame(void);
-
 ClientSideGame* mClientSideGame;
+
 };
 #endif
