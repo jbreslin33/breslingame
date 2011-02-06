06aa30e75889ea8dfe2c64fbaad896f75ec08c43
jbreslin33 <jbreslin33@localhost>
2011-02-06 13:49:42 -0500

    took out uneeded methods and vars.

--- game/serverSideGame.h
+++ game/serverSideGame.h
@@ -16,45 +16,32 @@ public:
 ServerSideGame();
 ~ServerSideGame();
 
+int               InitNetwork();
+void    	  ShutdownNetwork  (void);
+void    	  CalculateVelocity(ServerSideCommand *command, float frametime);
+void              MovePlayers      (void);
+void    	  MovePlayer       (ServerSideClient *client);
+void    	  AddClient        (void);
+void    	  RemoveClient	   (struct sockaddr *address);
+void   		  RemoveClients    (void);
+void    	  Frame            (int msec);
 
-int              InitNetwork();
-void    	 ShutdownNetwork  (void);
-void    	 CalculateVelocity(ServerSideCommand *command, float frametime);
-void             MovePlayers      (void);
-void    	 MovePlayer       (ServerSideClient *client);
-void    	 AddClient        (void);
-void    	 RemoveClient	  (struct sockaddr *address);
-void   		 RemoveClients    (void);
-void    	 Frame            (int msec);
+ServerSideClient* GetClientList	   (void)   { return clientList; }
 
-ServerSideClient *GetClientList	  (void)   { return clientList; }
+void    	 SetIndex          (int ind)       { index = ind; }
+int              GetIndex          (void)          { return index; }
 
-void    	 SetName          (char *n)                { strcpy(gamename, n); }
-char    	 *GetName         (void)                  { return gamename; }
-void    	 SetIndex         (int ind)       { index = ind; }
-int              GetIndex         (void)          { return index; }
+ServerSideNetwork* network;
+DreamServer*       networkServer;
+ServerSideGame*    next;
+ServerSideClient*  clientList;            // Client list
 
-
-	ServerSideNetwork* network;
-        DreamServer     *networkServer;
-        ServerSideGame *next;
-
-
-        ServerSideClient        *clientList;            // Client list
-        int             clients;                                // Number of clients
-
-        int             realtime;                               // Real server up-time in ms
-        int             servertime;                             // ServerSideGame frame * 100 ms
-        float   frametime;                              // Frame time in seconds
-
-        char    gamename[32];
-        int             index;
-
-        ServerSideClient *playerWithFlag;
-
-        long    framenum;
-
-
+int                clients;                                // Number of clients
+int                realtime;                               // Real server up-time in ms
+int                servertime;                             // ServerSideGame frame * 100 ms
+float              frametime;                              // Frame time in seconds
+int                index;
+long               framenum;
 
 };
 #endif
