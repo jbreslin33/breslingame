828ce9a7de9acf0c195a4ae0248c941e7e331ab1
jbreslin33 <jbreslin33@localhost>
2011-02-06 16:57:59 -0500

    ..had to use else for unix insted #ifdef UNIX don't know why.

--- basegame/baseGame.cpp
+++ basegame/baseGame.cpp
@@ -470,8 +470,10 @@ extern "C" {
 #endif
  
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+
     INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
 #else
+	#define UNIX
     int main(int argc, char *argv[])
 #endif
     {
