9be371261dba531d596bbc7ffe3487234e2c36bb
jbreslin33 <jbreslin33@localhost>
2011-02-12 17:42:15 -0500

    adding files

---
+++ shape/clientSideShape.h
@@ -0,0 +1,28 @@
+#ifndef CLIENTSIDESHAPE_H
+#define CLIENTSIDESHAPE_H
+
+#include "shape.h"
+
+#include "Ogre.h"
+#include "OIS.h"
+
+using namespace Ogre;
+
+class ClientSideShape : public Shape
+{
+
+public:
+
+ClientSideShape(Ogre::SceneManager* mSceneMgr, std::string name, int xPos, int yPos, int zPos, std::string mesh); 
+~ClientSideShape();
+
+
+void setupModel();
+
+//stats
+std::string mMeshName;
+
+};
+
+#endif
+
