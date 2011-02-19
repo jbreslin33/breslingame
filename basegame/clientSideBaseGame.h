945b7f2fc261e15ce1c25b7c1c718b7bf3924d27
jbreslin33 <jbreslin33@localhost>
2011-02-18 20:59:44 -0500

    ...

--- basegame/clientSideBaseGame.h
+++ basegame/clientSideBaseGame.h
@@ -12,16 +12,13 @@ public:
 ClientSideBaseGame(const char* serverIP);
 ~ClientSideBaseGame();
 
-bool processUnbufferedInput(const Ogre::FrameEvent& evt);
-
-//void         createPlayer(int index);
 virtual void createScene(void);
 virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
 virtual bool keyPressed( const OIS::KeyEvent &arg );
 
-OIS::Keyboard* getKeyBoard() { return  mKeyboard; }
+OIS::Keyboard*  getKeyBoard() { return  mKeyboard; }
 
-ClientSideGame*    mClientSideGame;
+ClientSideGame* mClientSideGame;
 
 const char* mServerIP;
 
