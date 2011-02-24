0ff2d296cd86ecb3903dca4c2a4c0cc488de6557
jbreslin33 <jbreslin33@localhost>
2011-02-24 11:13:56 -0500

    adding files.

--- tuts/BasicTutorial4.h
+++ tuts/BasicTutorial4.h
@@ -2,23 +2,23 @@
 -----------------------------------------------------------------------------
 Filename:    BasicTutorial4.h
 -----------------------------------------------------------------------------
- 
+
 This source file is part of the
-   ___                 __    __ _ _    _ 
+   ___                 __    __ _ _    _
   /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
  //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
 / \_// (_| | | |  __/  \  /\  /| |   <| |
 \___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
+      |___/
       Tutorial Framework
       http://www.ogre3d.org/tikiwiki/
 -----------------------------------------------------------------------------
 */
 #ifndef __BasicTutorial4_h_
 #define __BasicTutorial4_h_
- 
+
 #include "BaseApplication.h"
- 
+
 class BasicTutorial4 : public BaseApplication
 {
 public:
@@ -30,5 +30,5 @@ protected:
 private:
     bool processUnbufferedInput(const Ogre::FrameEvent& evt);
 };
- 
+
 #endif // #ifndef __BasicTutorial4_h_
