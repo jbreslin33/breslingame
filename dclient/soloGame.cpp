c86f6582a2b1ea428f5295f1c0fb2ecd737dc729
jbreslin33 <jbreslin33@localhost>
2011-03-02 19:14:04 -0500

    more polymorphism.

--- dclient/soloGame.cpp
+++ dclient/soloGame.cpp
@@ -6,3 +6,16 @@ SoloGame::SoloGame()
 SoloGame::~SoloGame()
 {
 }
+
+void SoloGame::createScene(void)
+{
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.75, 0.75, 0.75));
+
+    Ogre::Light* pointLight = mSceneMgr->createLight("pointLight");
+    pointLight->setType(Ogre::Light::LT_POINT);
+    pointLight->setPosition(Ogre::Vector3(250, 150, 250));
+    pointLight->setDiffuseColour(Ogre::ColourValue::White);
+    pointLight->setSpecularColour(Ogre::ColourValue::White);
+	        Ogre::Light* light = mSceneMgr->getLight("pointLight");
+        light->setVisible(true);
+}
