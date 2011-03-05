cd05313748ff292993af0ea1c2f5e5b2fe2229e5
jbreslin33 <jbreslin33@localhost>
2011-03-04 20:19:39 -0500

    moving on up.

--- dclient/networkedGame.h
+++ dclient/networkedGame.h
@@ -2,6 +2,7 @@
 #define NETOWORKEDGAME_H
 
 #include "game.h"
+#include "../tdreamsock/dreamSock.h"
 
 class NetworkedGame : public Game
 {
@@ -10,5 +11,19 @@ public:
 NetworkedGame();
 ~NetworkedGame();
 
+	void	ReadPackets(void);
+	void	ReadDeltaMoveCommand(dreamMessage *mes, clientData *client);
+	void	SendCommand(void);
+	void	SendRequestNonDeltaFrame(void);
+	void	ReadMoveCommand(dreamMessage *mes, clientData *client);
+
+	void	CheckPredictionError(int a);
+
+	void	Connect(void);
+	void	Disconnect(void);
+
+	void	BuildDeltaMoveCommand(dreamMessage *mes, clientData *theClient);
+	// Network variables
+	dreamClient *networkClient;
 };
 #endif
