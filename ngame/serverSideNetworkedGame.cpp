302d3b91c5fdafa61664d57dc0a5acdb22d57dff
jbreslin33 <jbreslin33@localhost>
2011-03-07 12:56:12 -0500

    just need to push stuff down to ServerSideNetworkedGame.

---
+++ ngame/serverSideNetworkedGame.cpp
@@ -0,0 +1,16 @@
+/******************************************/
+/* MMOG programmer's guide                */
+/* Tutorial game server                   */
+/* Programming:						      */
+/* Teijo Hakala						      */
+/******************************************/
+#include "serverSideNetworkedGame.h"
+
+ServerSideNetworkedGame::ServerSideNetworkedGame()
+{
+}
+
+ServerSideNetworkedGame::~ServerSideNetworkedGame()
+{
+}
+
---
+++ ngame/serverSideNetworkedGame.h
@@ -0,0 +1,15 @@
+#ifndef SERVERSIDENETWORKEDGAME_H
+#define SERVERSIDENETWORKEDGAME_H
+
+#include "serverSideGame.h"
+
+class ServerSideNetworkedGame : public ServerSideGame
+{
+
+public:
+	ServerSideNetworkedGame();
+	~ServerSideNetworkedGame();
+
+};
+
+#endif
