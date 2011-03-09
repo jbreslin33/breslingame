9a8e559d382d623c599f53995a97f4c8493e516c
jbreslin33 <jbreslin33@localhost>
2011-03-09 14:03:53 -0500

    moved everything to ServerSideNetworkGame.

--- ngame/serverSideGame.h
+++ ngame/serverSideGame.h
@@ -20,100 +20,15 @@
 #define USER_MES_SERVEREXIT			3
 
 
-typedef struct clientLoginData
-{
-	struct sockaddr		address;
-	dreamClient			*netClient;
-	clientLoginData		*next;
-} clientLoginData;
-
-typedef struct
-{
-	float x;
-	float y;
-} VECTOR2D;
-
-typedef struct
-{
-	int key;								// Pressed keys
-
-	VECTOR2D vel;							// Velocity
-	VECTOR2D origin;						// Position
-
-	int msec;								// How long to run command (in ms)
-
-} command_t;
-
-typedef struct clientData
-{
-	command_t frame[COMMAND_HISTORY_SIZE];
-	command_t serverFrame;
-	command_t command;
-
-	long processedFrame;
-
-	struct sockaddr address;
-	dreamClient *netClient;
-
-	VECTOR2D startPos;
-	bool team;
-
-	clientData *next;
-} clientData;
-
-
 class ServerSideGame
 {
 public:
-	dreamServer	*networkServer;
-
-	clientData	*clientList;		// Client list
-	int		clients;				// Number of clients
-
-	int		realtime;				// Real server up-time in ms
-	int		servertime;				// Server frame * 100 ms
-	float	frametime;				// Frame time in seconds
-
-	char	gamename[32];
-	int		index;
-
-	clientData *playerWithFlag;
-
-	long	framenum;
-
 
 	ServerSideGame();
 	~ServerSideGame();
 
-	// Network.cpp
-
 	virtual void ReadPackets() { bool donothing; }
-	void	SendCommand(void);
-	void	SendExitNotification(void);
-	void	ReadDeltaMoveCommand(dreamMessage *mes, clientData *client);
-	void	BuildMoveCommand(dreamMessage *mes, clientData *client);
-	void	BuildDeltaMoveCommand(dreamMessage *mes, clientData *client);
 
-	// Server.cpp
-
-	void	ShutdownNetwork(void);
-	void	CalculateVelocity(command_t *command, float frametime);
-	void	MovePlayers(void);
-	void	MovePlayer(clientData *client);
-	void	AddClient(void);
-	void	RemoveClient(struct sockaddr *address);
-	void	RemoveClients(void);
-	void	Frame(int msec);
-
-	clientData *GetClientList(void)	{ return clientList; }
-
-	void	SetName(char *n)		{ strcpy(gamename, n); }
-	char	*GetName(void)			{ return gamename; }
-
-	void	SetIndex(int ind)	{ index = ind; }
-	int		GetIndex(void)		{ return index; }
-
-	ServerSideGame *next;
 };
 
 #endif
